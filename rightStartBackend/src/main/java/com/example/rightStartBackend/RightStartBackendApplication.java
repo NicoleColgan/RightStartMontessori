90092a7c74abf04c142e1d2a212076fe038c1da7
Nicole Colgan <n.colgan3@nuigalway.ie>
2023-08-01 17:25:57 +0100

    deleting nested github account

---
+++ rightStartBackend/src/main/java/com/example/rightStartBackend/RightStartBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.rightStartBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RightStartBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RightStartBackendApplication.class, args);
+	}
+
+}
---
+++ rightStartBackend/src/test/java/com/example/rightStartBackend/RightStartBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rightStartBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RightStartBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
