a5f1875a71a0c7cee3c9c1cbd0c1b7836560fd47
Nicole Colgan <n.colgan3@nuigalway.ie>
2023-08-02 19:31:56 +0100

    adding resources

---
+++ rightstart-frontend/src/component/Landing.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "../App.css";
+
+
+const Landing = () => {
+  
+
+  return (
+    <div>
+        <div className='header'>
+        </div>
+    </div>
+    
+  )
+}
+
+export default Landing
