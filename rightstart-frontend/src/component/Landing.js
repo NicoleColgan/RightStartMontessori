ce9fbf04f089c47cb6642d2a20a636b62c73aec0
Nicole Colgan <n.colgan3@nuigalway.ie>
2023-08-03 21:30:25 +0100

    added image and started navbar

--- rightstart-frontend/src/component/Landing.js
+++ rightstart-frontend/src/component/Landing.js
@@ -2,13 +2,25 @@ import React from 'react'
 import "../App.css";
 
 
+
 const Landing = () => {
   
 
   return (
     <div>
-        <div className='header'>
-        </div>
+      <img src="/header.jpeg" class="headerImage"></img>
+      <div class="ribbon" >
+        
+        <nav class="navbar">
+          <img src="/writingOfLogo.png" class="writingOfLogo"></img>
+          <img src="/houseOfLogo.png" class="houseOfLogo"></img>
+         
+        </nav>
+      </div>
+        
+      <div style={{
+        height: "900px"
+      }}>dddddddddd</div>
     </div>
     
   )
